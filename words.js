e5e287069c5f86dc8471cc826ebcd71a5e83f316
Kaneki3119 <gilbert.esquilla.camacho@gmail.com>
2024-03-08 12:42:05 +0800

    Drag and drop game

---
+++ words.js
@@ -0,0 +1,62 @@
+const wordList = [
+    {
+        number:"1",
+        question: "Inimbetong akdang pasalaysay, karaniwang mahaba, masalimuot at itinatanghal ang karanasang pantao sa pamamagitan ng konektadong serye ng mga pangyayari.",
+        answer: "nobela",
+        choices: [
+            "dula",
+            "maikling",
+            "nobela",
+            "sanaysay"
+        ]
+    },
+
+
+    {
+        number:"2",
+        question: "Ang nobelang 'Takipsilim sa Diyakarta' ay mula sa bansang ______________",
+        answer: "Indonesia",
+        choices: [
+            "Pilipinas",
+            "Singapore",
+            "Indonesia",
+            "Vietnam"
+        ]
+    },
+
+    {
+        number:"3",
+        question: "Sino sa mga tauhan ng nobela ang nagbibigay inspirasyon sa mga mahihirap upang hindi sila apihin?",
+        answer: "Andres",
+        choices: [
+            "Andres",
+            "Bill",
+            "Alice",
+            "Ama"
+        ]
+    },
+
+    {
+        number:"4",
+        question: "Ano ang naging adhikain ng ama ni Andres sa nobelang 'Timawa'?",
+        answer: "Maging manggagamot",
+        choices: [
+            "Maging enhinyero",
+            "Maging manggagamot",
+            "Maging guro",
+            "Maging piloto"
+        ]
+    },
+
+    {
+        number:"5",
+        question: "Ano ang naging hanapbuhay ni Andres sa Amerika?",
+        answer: "Paghuhugas ng pinggan",
+        choices: [
+            "Pagwawalis",
+            "Pagsasayaw",
+            "Paghuhugas ng pinggan",
+            "Pagtitinda ng prutas"
+        ]
+    }
+];
